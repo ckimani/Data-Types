bbfa670a0b937e12ef3d84fe41e8f70f977285b3
ckimani <ckimani32@gmail.com>
2017-01-19 16:51:12 +0300

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,32 @@
+class BinarySearch(ListComprehension):
+  '''
+  Child class BinarySearch
+  '''
+  def search(self, item):
+		self.item=item
+		dictionary={}
+	
+		if self.a==0:
+			return False 
+		else:
+			dictionary[item]+=1
+			
+			###########################
+			
+class ListComprehension(object):
+  '''
+			Implementation of the parent class
+			'''
+	list=[]
+	length=None
+	def _init_(self, a,b):
+		self.a=len(list)
+		self.b = difference
+		self.lenght.list()
+
+	def list(self):
+		for index, number in enumerate(self.a):
+			if index< self.length-1:
+				self.a[index+1]-self.a[index]==1
+				list.append(number)
+		return list
